5dbfa94da9b1a120628f096bb2dd24afa8ed12d3
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2022-10-29 16:39:24 -0400

    Add fixture for injecting operation into DB before running test

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,8 +3,48 @@
 from typing import Iterator
 import pytest
 
+import dba
+
 
 @pytest.fixture
 def base_url() -> Iterator[str]:
     """URL to flask server in composable environment."""
     yield "http://server"
+
+
+@pytest.fixture(name="uuid")
+def uuid_fixture() -> Iterator[int]:
+    """Yield the uuid used for testing."""
+    yield 1234
+
+
+@pytest.fixture(name="operation")
+def operation_fixture(uuid: int) -> Iterator[str]:
+    """Bind uuid to operation string in DB."""
+    with dba.get_cursor(autocommit=True) as cursor:
+        operation = "identity"
+        query = """
+            INSERT
+                INTO operations
+                (
+                    uuid,
+                    operation
+                )
+            VALUES
+                (
+                    %(uuid)s,
+                    %(operation)s
+                )
+        """
+        cursor.execute(query, {"uuid": uuid, "operation": operation})
+
+    yield operation
+
+    with dba.get_cursor(autocommit=True) as cursor:
+        query = """
+            DELETE
+                FROM operations
+            WHERE
+                uuid=%(uuid)s
+        """
+        cursor.execute(query, {"uuid": uuid})
--- tests/integration/test_compute.py
+++ tests/integration/test_compute.py
@@ -3,11 +3,13 @@
 import json
 import requests
 
+import pytest
 
-def test_compute_identity(base_url: str) -> None:
+
+@pytest.mark.usefixtures("operation")
+def test_compute_identity(base_url: str, uuid: int) -> None:
     """Test the /compute end-point for a user with identity operation."""
     # GIVEN
-    uuid = 1234
     input_value = 9
     url = f"{base_url}/compute"
     payload = {"uuid": uuid, "input": input_value}
