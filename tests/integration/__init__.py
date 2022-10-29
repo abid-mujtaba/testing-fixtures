953cbe339c3cc749c044698ba1fd799ebdacf84d
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2022-10-29 18:42:08 -0400

    Implement tunable fixture

---
+++ tests/integration/__init__.py
@@ -0,0 +1,66 @@
+"""Shared package utilities for testing."""
+
+from functools import partial, wraps
+from typing import Callable, ParamSpec
+
+import dba
+
+
+P = ParamSpec("P")
+
+UUID = 1234
+
+
+def inject_operation(uuid: int, operation_name: str) -> None:
+    """Inject specified uuid and operation into DB."""
+    with dba.get_cursor(autocommit=True) as cursor:
+        query = """
+            INSERT
+                INTO operations
+                (
+                    uuid,
+                    operation
+                )
+            VALUES
+                (
+                    %(uuid)s,
+                    %(operation)s
+                )
+        """
+        cursor.execute(query, {"uuid": uuid, "operation": operation_name})
+
+
+def uninject_operation(uuid: int) -> None:
+    """Remove injected operation from DB."""
+    with dba.get_cursor(autocommit=True) as cursor:
+        query = """
+            DELETE
+                FROM operations
+            WHERE
+                uuid=%(uuid)s
+        """
+        cursor.execute(query, {"uuid": uuid})
+
+
+def operation(operation_name: str) -> Callable[[Callable[P, None]], Callable[P, None]]:
+    """Generate a decorator that injects specified operation into DB."""
+
+    def decorator(func: Callable[P, None]) -> Callable[P, None]:
+        """A decorator that injects operation into DB and associated uuid into test function."""
+
+        new_func = partial(func, uuid=UUID)
+
+        @wraps(new_func)
+        def inner(*args: P.args, **kwargs: P.kwargs) -> None:
+            """Inner function of decorator for wrapping."""
+            try:
+                inject_operation(UUID, operation_name)
+
+                new_func(*args, **kwargs)
+
+            finally:
+                uninject_operation(UUID)
+
+        return inner
+
+    return decorator
--- tests/integration/test_compute.py
+++ tests/integration/test_compute.py
@@ -3,22 +3,20 @@
 import json
 import requests
 
-import pytest
+from . import operation
 
 
-@pytest.mark.usefixtures("operation")
+@operation("identity")
 def test_compute_identity(base_url: str, uuid: int) -> None:
     """Test the /compute end-point for a user with identity operation."""
     # GIVEN
-    input_value = 9
-    url = f"{base_url}/compute"
-    payload = {"uuid": uuid, "input": input_value}
+    payload = {"uuid": uuid, "input": 9}
 
     # WHEN
-    response = requests.post(url, json=payload, timeout=5)
+    response = requests.post(f"{base_url}/compute", json=payload, timeout=5)
 
     # THEN
     assert response.status_code == 200
 
     output = json.loads(response.text)
-    assert output["result"] == input_value  # Identity operation
+    assert output["result"] == 9  # Identity operation
