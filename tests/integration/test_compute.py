f719d6307173b15948115b59c0462ee98fce5701
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2022-10-29 21:06:55 -0400

    Add remaining tests for operations including a missing one

--- tests/integration/test_compute.py
+++ tests/integration/test_compute.py
@@ -20,3 +20,53 @@ def test_compute_identity(base_url: str, uuid: int) -> None:
 
     output = json.loads(response.text)
     assert output["result"] == 9  # Identity operation
+
+
+@operation("square")
+def test_compute_square(base_url: str, uuid: int) -> None:
+    """Test the /compute end-point for a user with square operation."""
+    # GIVEN
+    payload = {"uuid": uuid, "input": 9}
+
+    # WHEN
+    response = requests.post(f"{base_url}/compute", json=payload, timeout=5)
+
+    # THEN
+    assert response.status_code == 200
+
+    output = json.loads(response.text)
+    assert output["result"] == 81  # square operation
+
+
+@operation("cube")
+def test_compute_cube(base_url: str, uuid: int) -> None:
+    """Test the /compute end-point for a user with cube operation."""
+    # GIVEN
+    payload = {"uuid": uuid, "input": 9}
+
+    # WHEN
+    response = requests.post(f"{base_url}/compute", json=payload, timeout=5)
+
+    # THEN
+    assert response.status_code == 200
+
+    output = json.loads(response.text)
+    assert output["result"] == 729  # cube operation
+
+
+# Note: No operation record in the DB
+def test_compute_no_operation(base_url: str) -> None:
+    """Test the /compute end-point for a user with no operation in the DB."""
+    # GIVEN
+    uuid = 7890
+    payload = {"uuid": uuid, "input": 9}
+
+    # WHEN
+    response = requests.post(f"{base_url}/compute", json=payload, timeout=5)
+
+    # THEN
+    assert response.status_code == 200
+
+    output = json.loads(response.text)
+    assert "error" in output
+    assert str(uuid) in output["error"]["message"]
