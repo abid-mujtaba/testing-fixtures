a33b826945ae691b4cb7bc22db942cda27d4b78b
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2022-11-05 10:32:51 -0400

    Add usage of a fixture parametrized from the test function decoration site

--- tests/unit/test_new_fixtures.py
+++ tests/unit/test_new_fixtures.py
@@ -1,9 +1,16 @@
 """Test the new fixtures."""
 
 from .utils import fixture_a, TypeA
+from .utils import fixture_b, TypeB
 
 
 @fixture_a
 def test_a(val_a: TypeA) -> None:
-    """Test fixture_A in isolation."""
+    """Test fixture_a in isolation."""
     assert val_a == "Value A"
+
+
+@fixture_b(TypeB("Value B"))
+def test_b(val_b: TypeB) -> None:
+    """Test parametrized fixture_b in isolation."""
+    assert val_b == "Injected Value B"
--- tests/unit/utils.py
+++ tests/unit/utils.py
@@ -6,9 +6,16 @@ from .fixtures import fixture
 
 
 TypeA = NewType("TypeA", str)
+TypeB = NewType("TypeB", str)
 
 
 @fixture
 def fixture_a() -> Iterator[TypeA]:
     """A simple fixture that yields a string."""
     yield TypeA("Value A")
+
+
+@fixture
+def fixture_b(val_b: TypeB) -> Iterator[TypeB]:
+    """A fixture that takes injected value from the test function decoration."""
+    yield TypeB(f"Injectd {val_b}")
