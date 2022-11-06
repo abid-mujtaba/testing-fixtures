2f4c7e407da1332fddea620ca0e308e6fd1b1fc3
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2022-11-05 22:17:35 -0400

    Add example of fixture that takes injections from both another fixture and the test site

--- tests/unit/test_new_fixtures.py
+++ tests/unit/test_new_fixtures.py
@@ -3,6 +3,7 @@
 from .utils import fixture_a, TypeA
 from .utils import fixture_b, TypeB1, TypeB2, TypeB3
 from .utils import fixture_c, TypeC
+from .utils import fixture_d, TypeD1, TypeD2
 
 
 @fixture_a()
@@ -14,13 +15,22 @@ def test_a(val_a: TypeA) -> None:
 @fixture_b(TypeB1(42), TypeB2(3.14))
 def test_b(val_b: TypeB3) -> None:
     """Test parametrized fixture_b in isolation."""
-    assert val_b == "Injected into fixture b 42 and 3.14"
+    assert val_b == "Injected into fixture_b 42 and 3.14"
 
 
 @fixture_c()  # pylint: disable=E1120
 def test_c(val_c: TypeC) -> None:
     """Test fixture_c which receives values from fixture_b."""
-    assert val_c == "Injected into fixture_C: Injected into fixture b 13 and 1.44"
+    assert val_c == "Injected into fixture_c: Injected into fixture_b 13 and 1.44"
+
+
+@fixture_d(TypeD1(True))  # pylint: disable=E1120
+def test_d(val_d: TypeD2) -> None:
+    """Test fixture_d which receives values from both fixture_b and the test site."""
+    assert (
+        val_d == "Injected into fixture_d from both b: "
+        "Injected into fixture_b 123 and 1.23 and test site: True"
+    )
 
 
 # @fixture_b(TypeB("Value B"), inject=False)
--- tests/unit/utils.py
+++ tests/unit/utils.py
@@ -13,6 +13,9 @@ TypeB3 = NewType("TypeB3", str)
 
 TypeC = NewType("TypeC", str)
 
+TypeD1 = NewType("TypeD1", bool)
+TypeD2 = NewType("TypeD2", str)
+
 
 @fixture
 def fixture_a() -> Iterator[TypeA]:
@@ -23,11 +26,22 @@ def fixture_a() -> Iterator[TypeA]:
 @fixture
 def fixture_b(val_b1: TypeB1, val_b2: TypeB2) -> Iterator[TypeB3]:
     """A fixture that takes injected value from the test function decoration."""
-    yield TypeB3(f"Injected into fixture b {val_b1} and {val_b2}")
+    yield TypeB3(f"Injected into fixture_b {val_b1} and {val_b2}")
 
 
 @fixture
 @fixture_b(TypeB1(13), TypeB2(1.44))
 def fixture_c(val_b: TypeB3) -> Iterator[TypeC]:
     """A fixture that takes an injected value from ANOTHER fixture."""
-    yield TypeC(f"Injected into fixture_C: {val_b}")
+    yield TypeC(f"Injected into fixture_c: {val_b}")
+
+
+@fixture
+@fixture_b(TypeB1(123), TypeB2(1.23))
+def fixture_d(val_b: TypeB3, val_d: TypeD1) -> Iterator[TypeD2]:
+    """
+    A fixture that takes injected value from two places.
+
+    From both the test function site AND from ANOTHER fixture.
+    """
+    yield TypeD2(f"Injected into fixture_d from both b: {val_b} and test site: {val_d}")
