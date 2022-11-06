da149c8b646fd6800dc0b677d23c24f3b4cd3a7a
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2022-11-05 23:25:41 -0400

    Partial refactor away from strings to TypedDict showing injection cleanly

--- tests/unit/test_new_fixtures.py
+++ tests/unit/test_new_fixtures.py
@@ -2,66 +2,67 @@
 
 import pytest
 
-from .utils import fixture_a, TypeA
-from .utils import fixture_b, TypeB1, TypeB2, TypeB3
-from .utils import fixture_c, TypeC
-from .utils import fixture_d, TypeD1, TypeD2
-from .utils import fixture_e, TypeE
-from .utils import fixture_f, TypeF1, TypeF2
+from .utils import fixture_a, A
+from .utils import fixture_b, Bi1, Bi2, Bo
+
+# from .utils import fixture_c, TypeC
+# from .utils import fixture_d, TypeD1, TypeD2
+# from .utils import fixture_e, TypeE
+# from .utils import fixture_f, TypeF1, TypeF2
 
 
 @fixture_a()
-def test_a(val_a: TypeA) -> None:
+def test_a(a: A) -> None:
     """Test fixture_a in isolation."""
-    assert val_a == "Value A"
+    assert a == "a"
 
 
-@fixture_b(TypeB1(42), TypeB2(3.14))
-def test_b(val_b: TypeB3) -> None:
+@fixture_b(Bi1(42), Bi2(3.14))
+def test_b(b: Bo) -> None:
     """Test parametrized fixture_b in isolation."""
-    assert val_b == "Injected into fixture_b 42 and 3.14"
+    assert b == {"b1": 42, "b2": 3.14}
 
 
-@fixture_c()  # pylint: disable=E1120
-def test_c(val_c: TypeC) -> None:
-    """Test fixture_c which receives values from fixture_b."""
-    assert val_c == "Injected into fixture_c: Injected into fixture_b 13 and 1.44"
+# @fixture_c()  # pylint: disable=E1120
+# def test_c(val_c: TypeC) -> None:
+#     """Test fixture_c which receives values from fixture_b."""
+#     assert val_c == "Injected into fixture_c: Injected into fixture_b 13 and 1.44"
 
 
-@fixture_d(TypeD1(True))  # pylint: disable=E1120
-def test_d(val_d: TypeD2) -> None:
-    """Test fixture_d which receives values from both fixture_b and the test site."""
-    assert (
-        val_d == "Injected into fixture_d from both b: "
-        "Injected into fixture_b 123 and 1.23 and test site: True"
-    )
+# @fixture_d(TypeD1(True))  # pylint: disable=E1120
+# def test_d(val_d: TypeD2) -> None:
+#     """Test fixture_d which receives values from both fixture_b and the test site."""
+#     assert (
+#         val_d == "Injected into fixture_d from both b: "
+#         "Injected into fixture_b 123 and 1.23 and test site: True"
+#     )
 
 
-@fixture_b(TypeB1(88), TypeB2(12.3))
-@fixture_a()
-def test_a_and_b(val_a: TypeA, val_b: TypeB3) -> None:
-    """Test application of two fixtures to one test."""
-    assert val_a == "Value A"
-    assert val_b == "Injected into fixture_b 88 and 12.3"
+# @fixture_b(TypeB1(88), TypeB2(12.3))
+# @fixture_a()
+# def test_a_and_b(val_a: TypeA, val_b: TypeB3) -> None:
+#     """Test application of two fixtures to one test."""
+#     assert val_a == "Value A"
+#     assert val_b == "Injected into fixture_b 88 and 12.3"
 
 
-@fixture_e()
-def test_e(val_e: TypeE) -> None:
-    """Test mutation of injected state by fixture_e."""
-    assert val_e == 1
+# @fixture_e()
+# def test_e(val_e: TypeE) -> None:
+#     """Test mutation of injected state by fixture_e."""
+#     assert val_e == 1
 
 
-@pytest.mark.skip
-@fixture_f(TypeF1(42))  # pylint: disable=E1120
-@fixture_e()
-def test_fixture_f(val_e: TypeE, val_f: TypeF2) -> None:
-    """Test fixture_f and double injecion of fixture_e."""
-    assert val_e == 1
-    assert val_f == (
-        "Injected into fixture_f values from fixture_e 1 and from test site 42"
-    )
+# @pytest.mark.skip
+# @fixture_f(TypeF1(42))  # pylint: disable=E1120
+# @fixture_e()
+# def test_fixture_f(val_e: TypeE, val_f: TypeF2) -> None:
+#     """Test fixture_f and double injecion of fixture_e."""
+#     assert val_e == 1
+#     assert val_f == (
+#         "Injected into fixture_f values from fixture_e 1 and from test site 42"
+#     )
 
 
-# @fixture_b(TypeB("Value B"), inject=False)
-# def test_b_no_injection() -> None:
-#     """The value yielded by fixture_b is NOT injected into the test."""
+# # @fixture_b(TypeB("Value B"), inject=False)
+# # def test_b_no_injection() -> None:
+# #     """The value yielded by fixture_b is NOT injected into the test."""
--- tests/unit/utils.py
+++ tests/unit/utils.py
@@ -1,76 +1,82 @@
 """Define fixtures for these tests."""
 
-from typing import Iterator, NewType
+from typing import Iterator, NewType, TypedDict
 
 from .fixtures import fixture
 
 
-TypeA = NewType("TypeA", str)
+A = NewType("A", str)
 
 
 @fixture
-def fixture_a() -> Iterator[TypeA]:
+def fixture_a() -> Iterator[A]:
     """A simple fixture that yields a string."""
-    yield TypeA("Value A")
+    yield A("a")
 
 
-TypeB1 = NewType("TypeB1", int)
-TypeB2 = NewType("TypeB2", float)
-TypeB3 = NewType("TypeB3", str)
+Bi1 = NewType("Bi1", int)
+Bi2 = NewType("Bi2", float)
+
+
+class Bo(TypedDict):
+    """Output type for fixture_b encapsulating the two inputs."""
+
+    b1: Bi1
+    b2: Bi2
 
 
 @fixture
-def fixture_b(val_b1: TypeB1, val_b2: TypeB2) -> Iterator[TypeB3]:
+def fixture_b(b1: Bi1, b2: Bi2) -> Iterator[Bo]:
     """A fixture that takes injected value from the test function decoration."""
-    yield TypeB3(f"Injected into fixture_b {val_b1} and {val_b2}")
+    yield Bo(b1=b1, b2=b2)
 
 
-TypeC = NewType("TypeC", str)
+# TypeC = NewType("TypeC", str)
 
 
-@fixture
-@fixture_b(TypeB1(13), TypeB2(1.44))
-def fixture_c(val_b: TypeB3) -> Iterator[TypeC]:
-    """A fixture that takes an injected value from ANOTHER fixture."""
-    yield TypeC(f"Injected into fixture_c: {val_b}")
+# @fixture
+# @fixture_b(TypeB1(13), TypeB2(1.44))
+# def fixture_c(val_b: TypeB3) -> Iterator[TypeC]:
+#     """A fixture that takes an injected value from ANOTHER fixture."""
+#     yield TypeC(f"Injected into fixture_c: {val_b}")
 
 
-TypeD1 = NewType("TypeD1", bool)
-TypeD2 = NewType("TypeD2", str)
+# TypeD1 = NewType("TypeD1", bool)
+# TypeD2 = NewType("TypeD2", str)
 
 
-@fixture
-@fixture_b(TypeB1(123), TypeB2(1.23))
-def fixture_d(val_b: TypeB3, val_d: TypeD1) -> Iterator[TypeD2]:
-    """
-    A fixture that takes injected value from two places.
+# @fixture
+# @fixture_b(TypeB1(123), TypeB2(1.23))
+# def fixture_d(val_b: TypeB3, val_d: TypeD1) -> Iterator[TypeD2]:
+#     """
+#     A fixture that takes injected value from two places.
 
-    From both the test function site AND from ANOTHER fixture.
-    """
-    yield TypeD2(f"Injected into fixture_d from both b: {val_b} and test site: {val_d}")
-
-
-TypeE = NewType("TypeE", int)
-VALUE_E = {"value": 0}
+#     From both the test function site AND from ANOTHER fixture.
+#     """
+#     yield TypeD2(f"Injected into fixture_d from both b: {val_b} and test site: {val_d}")
 
 
-@fixture
-def fixture_e() -> Iterator[TypeE]:
-    """A fixture that mutates the inject value before yielding it."""
-    VALUE_E["value"] += 1
-
-    yield TypeE(VALUE_E["value"])
+# TypeE = NewType("TypeE", int)
+# VALUE_E = {"value": 0}
 
 
-TypeF1 = NewType("TypeF1", int)
-TypeF2 = NewType("TypeF2", str)
+# @fixture
+# def fixture_e() -> Iterator[TypeE]:
+#     """A fixture that mutates the inject value before yielding it."""
+#     VALUE_E["value"] += 1
+
+#     yield TypeE(VALUE_E["value"])
 
 
-@fixture
-@fixture_e()
-def fixture_f(val_e: TypeE, val_f: TypeF1) -> Iterator[TypeF2]:
-    """A fixture that gets state from both test site and fixture_e (mutating)."""
-    yield TypeF2(
-        f"Injected into fixture_f values from fixture_e {val_e} and "
-        f"from test site {val_f}"
-    )
+# TypeF1 = NewType("TypeF1", int)
+# TypeF2 = NewType("TypeF2", str)
+
+
+# @fixture
+# @fixture_e()
+# def fixture_f(val_e: TypeE, val_f: TypeF1) -> Iterator[TypeF2]:
+#     """A fixture that gets state from both test site and fixture_e (mutating)."""
+#     yield TypeF2(
+#         f"Injected into fixture_f values from fixture_e {val_e} and "
+#         f"from test site {val_f}"
+#     )
